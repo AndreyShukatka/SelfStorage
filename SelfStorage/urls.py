ab6a4f03e709c8c6dc16131b659deb66d2e7fc5e
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2023-04-18 22:11:46 +0300

    Генератор QR кода

--- SelfStorage/urls.py
+++ SelfStorage/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from qrgenerator.views import qrgenerator
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sitestorage.urls'))
+    path('', include('sitestorage.urls')),
+    path('qr/', qrgenerator, name='qr-generator')
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- qrgenerator/views.py
+++ qrgenerator/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import qrcode
 
-# Create your views here.
+def qrgenerator(request):
+    qr_image = False
+    if request.method == "POST":
+        data = request.POST['data']
+        img = qrcode.make(data)
+        img.save("media/qr/test.png")
+        qr_image = True
+    return render(request, 'qrgenerator/qr.html', {'qr_image': qr_image})
