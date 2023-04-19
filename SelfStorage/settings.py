e69e7cef99fe704f1ccac22a14373c4a1ffc0874
Sergey <sergey.bv26@gmail.com>
2023-04-19 22:40:57 +0300

    add requirements and env.sample

--- SelfStorage/settings.py
+++ SelfStorage/settings.py
@@ -89,4 +89,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
