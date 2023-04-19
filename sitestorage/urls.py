13681ed5bc2eb52a7e18838225f0c4171133c65f
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2023-04-19 12:03:07 +0300

    Почистил импорты

--- sitestorage/urls.py
+++ sitestorage/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from sitestorage import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -11,5 +10,3 @@ urlpatterns = [
     path('my-rent-empty', views.my_rent_empty, name='my_rent_empty')
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
