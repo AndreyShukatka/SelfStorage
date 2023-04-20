3b38f6c26cfe39cbecb56f01d43636db59fad648
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2023-04-20 23:25:22 +0300

    Сделал авторизацию на сайте

--- sitestorage/views.py
+++ sitestorage/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm # UserAuthorizationForm,
+from .forms import UserRegistrationForm, UserAuthorizationForm
 from django.contrib.auth import authenticate, login
-
 
 def index(request):
     user_registration_form = UserRegistrationForm()
-    # user_login_form = UserAuthorizationForm()
+    user_login_form = UserAuthorizationForm()
     context = {
         'user_registration_form': user_registration_form,
-        # 'user_login_form': user_login_form
+        'user_login_form': user_login_form
     }
     registration(request)
+    authorize(request)
     return render(request, 'index.html', context=context)
 
 
@@ -42,22 +42,25 @@ def my_rent_empty(request):
 
 def registration(request):
     if request.method == 'POST':
-        if request.POST.get('value') == 'registration':
+        if request.POST.get('type') == 'registration':
             user_registration_form = UserRegistrationForm(request.POST)
             if user_registration_form.is_valid():
                 new_user = user_registration_form.save(commit=False)
                 new_user.set_password(user_registration_form.cleaned_data['password'])
                 new_user.save()
-                return render(request, 'index.html', {'new_user': new_user})
-            else:
-                user_registration_form = UserRegistrationForm()
-                return render(request, 'index.html', {'user_registration_form': user_registration_form})
+                email = request.POST.get('email')
+                password = request.POST.get('password')
+                user = authenticate(email=email, password=password)
+                login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+                return redirect('index')
     else:
         pass
 
 
-# def authorize(request):
-#     if request.method == 'POST':
-#         if request.POST.get('value') == 'login':
-#             email =
-#             user = authenticate(email='john', password='secret')
+def authorize(request):
+    if request.method == 'POST':
+        if request.POST.get('type') == 'login':
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = authenticate(email=email, password=password)
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
